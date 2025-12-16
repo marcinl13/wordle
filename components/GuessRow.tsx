587570af2587e57858762b7859e9c0095e6ce291
marlepo <marcinlpotrykus@gmail.com>
2025-12-16 18:29:27 +0100

    feat: guess row with status evaluation

---
+++ components/GuessRow.tsx
@@ -0,0 +1,18 @@
+import { evaluateStatus } from '@/lib/evaluateStatus';
+import GuessTile from './GuessTile';
+
+type Props = {
+  guess?: string;
+  dailyWord: string;
+};
+
+export default function GuessRow({ guess = '', dailyWord }: Props) {
+  const tiles = Array.from({ length: dailyWord.length }).map((_, i) => {
+    const letter = guess[i] || '';
+    const status = evaluateStatus(guess, dailyWord, i);
+
+    return <GuessTile key={i} letter={letter} status={status} />;
+  });
+
+  return <div className="flex gap-2">{tiles}</div>;
+}
