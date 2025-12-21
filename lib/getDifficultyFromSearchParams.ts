9e99c374d26558a522573cf2dc4e7ad6256c4a89
marlepo <marcinlpotrykus@gmail.com>
2025-12-21 15:37:02 +0100

    feat: getDifficultyFromSearchParams

---
+++ lib/getDifficultyFromSearchParams.ts
@@ -0,0 +1,19 @@
+export type Difficulty = "normal" | "hard";
+
+type SearchParams = Record<string, string | string[] | undefined>;
+
+export function getDifficultyFromSearchParams(
+  params: SearchParams,
+): Difficulty {
+  if (!params) return "normal";
+
+  if (params.hasOwnProperty("difficulty")) {
+    const difficulty = params["difficulty"];
+
+    if (difficulty === "hard") return "hard";
+
+    return "normal";
+  }
+
+  return "normal";
+}
