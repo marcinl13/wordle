3724ec00599dca3c2ad526677198fd25ca424079
marlepo <marcinlpotrykus@gmail.com>
2025-12-27 13:18:18 +0100

    feat: display difficulty

--- app/daily-guess/page.tsx
+++ app/daily-guess/page.tsx
@@ -12,6 +12,9 @@ export default async function Home({
   return (
     <main className="container">
       <h1 className="text-4xl font-bold text-center">Daily Word Guess</h1>
+      <h2 className="text-2xl font-bold text-center text-orange-400 uppercase">
+        {difficulty}
+      </h2>
 
       <Game dailyWord={dailyWord} difficulty={difficulty} />
     </main>
